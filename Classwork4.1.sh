c045b30e3eeb4d83fa8358cce455f850718d48b7
Your Name <vandergriffbryce@gmail.com>
2022-09-20 08:23:08 -0400

    modified:   Classwork4.1.sh     changed number logic to be more 'effcient'

--- Classwork4.1.sh
+++ Classwork4.1.sh
@@ -10,7 +10,7 @@
 # 3. If there is any one of the option is invalid, you should not display 
 #    valid options. For example if the options pdi since i is invalid, you 
 #    should not display answer for p and d. should not be any valid answers.
-# 4. At least 8 different types of error codes
+# 4. At least 8 different types of error codes 
 # 3. Add functionality to place digit for -d anywhere (3 -d -p -u)
 ####################################################
 
@@ -26,74 +26,50 @@ clear='\033[0m'
 min=1
 max=7
 
-U=0
-P=0
-D=0
-error=0
-index=0
+U=0 ; P=0 ; D=0 ; error=0 ; index=0
 
 errorstring="-ERROR CODE"
 
 optspec=":upd:-:"
 while getopts "$optspec" optchar; do
+    echo ${optchar} | tr 'A-Z' 'a-z'            #doesnt take uppercase letters
     case "${optchar}" in
         -)
             case "${OPTARG}" in
                 day)
-                    ((D+=1))
-                #    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-                #    echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
-                    ;;
-                #day=*)
-                #    val=${OPTARG#*=}
-                #    opt=${OPTARG%=$val}
-                #    echo "Parsing option: '--${opt}', value: '${val}'" >&2
-                #    ;;
+                    ((D+=1));;
                 username)
-                    ((U+=1))
-                    ;;
+                    ((U+=1));;
                 print)
-                    ((P+=1))
-                    ;;
-                *)
-                    if [ "$OPTERR" = 1 ] && [ "${optspec:0:1}" != ":" ]; then
-                        echo "Unknown option --${OPTARG}" >&2
-                    fi
-                    ;;
+                    ((P+=1));;
             esac;;
         u)
-            ((U+=1))
-            ;;
+            ((U+=1));;
         p)
-            ((P+=1))
-            ;;
+            ((P+=1));;
         d)
-            ((D+=1))
+            ((D+=1));;
              #echo "-d was triggered, Parameter: $OPTARG" >&2            #debugging
-            if [[ $OPTARG = *[[:digit:]]* ]] && [ "$OPTARG" -le "$max" ] && [ "$OPTARG" -ge "$min" ];        #tests for digit and >, <, array limits
-            then
-                #is number
-                echo ${weekdays[$OPTARG]}
-
-            else
-                #not number
-                echo -e "${red}-ERROR CODE 1-${clear}"                                                      #ERROR CODE 1
-                echo -e "${red}INVALID ARGUMENT: Valid arguemts '1-7'${clear}" 
-            fi
-            shift;;
+            #if [[ $OPTARG = *[[:digit:]]* ]] && [ "$OPTARG" -le "$max" ] && [ "$OPTARG" -ge "$min" ];        #tests for digit and >, <, array limits
+            #then
+            #    #is number
+            #    echo ${weekdays[$OPTARG]}
+            #
+            #else
+            #    #not number
+            #    echo -e "${red}-ERROR CODE 1-${clear}"                                                      #ERROR CODE 1
+            #    echo -e "${red}INVALID ARGUMENT: Valid arguemts '1-7'${clear}" 
+            #fi
+            #shift
     
         *)
-            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then #catches if d has no int need to fix
+            if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then                                       #catches if d has no int need to fix
                 echo -e "${red}-ERROR CODE 2-${clear}"                                                      #ERROR CODE 2
                 echo -e "${red}Non-option argument: '-${OPTARG}'${clear}" >&2
             fi
             ;;
     esac
 done
-
-if [ $D -eq 1 ]; then
-    echo ${weekdays[$OPTARG]}
-fi
 
 if [ $U -eq 1 ]; then
     echo $USER
@@ -105,6 +81,22 @@ if [ $P -eq 1 ]; then
     echo $PWD
 elif [ $P -gt 1 ]; then
     error=6
+fi
+
+if [ $D -eq 1 ]; then
+    for var in $*; do
+        if [[ $var = *[[:digit:]]* ]] && [ "$var" -le "$max" ] && [ "$var" -ge "$min" ];        #skips over other flags ((-d -p 1) p doesnt work)
+            then
+                #is number
+                echo ${weekdays[$var]}              
+
+            #else
+                #not number
+            #   error=1
+        fi
+    done
+elif [ $D -gt 1 ]; then         #doesnt hit when more than one d 
+    error=8
 fi
 
 if [ $# -eq 0 ]; then
@@ -119,7 +111,7 @@ case $error in
     5) echo $errorstring" 5-"$'\n'"Accepted arguments: -u -d 'INT' -p"; exit 1;;
     6) echo $errorstring" 6-"$'\n'"More than one type of -p used"; exit 1;;
     7) echo $errorstring" 7-"$'\n'"More than one type of -u used"; exit 1;;
-    8) echo $errorstring" 8-"$'\n'""; exit 1;;
+    8) echo $errorstring" 8-"$'\n'"More than one tyoe if -d used"; exit 1;;
 esac
 
 echo "thank you for using my program"
