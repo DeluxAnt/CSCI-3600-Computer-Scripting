cd336ec327f29cc7361adcde7a52b8cbe1ac6e55
DeluxAnt <vandergriffbryce@gmail.com>
2022-09-21 23:04:28 -0400

    Completed 5.2 #1 Spec

--- Classwork5.2.sh
+++ Classwork5.2.sh
@@ -1,4 +1,13 @@
+####################################################
+#Filename: Classwork5.2.sh
+#Author: Anthony Vandergriff (VANDERGRIFF@ETSU.EDU)
+#Class: CSCI-3600-001 Computer Scripting
+#Created: 9/21/2022
+#Updated: 9/21/2022
 #!/bin/bash
 inet=$(ifconfig | grep -i "inet ")
-echo "IPv4 Adresses:"
+inet6=$(ifconfig | grep -i "inet6")
+echo "IPv4 Addresses:"
 echo "$inet"
+echo "IPv6 Addresses"
+echo "$inet6"
