4e64dce267956c6bab9f3256e7a504bf12160f3e
DeluxAnt <vandergriffbryce@gmail.com>
2022-11-10 23:12:26 -0500

    modified:   FinalProject/Notes.txt 	modified:   FinalProject/asciify.sh 	new file:   FinalProject/right-arrow.png

--- FinalProject/asciify.sh
+++ FinalProject/asciify.sh
@@ -15,3 +15,36 @@ hight=$(echo $size | awk 'BEGIN { FS = "x" } ; { print $1 }')
 width=$(echo $size | awk 'BEGIN { FS = "x" } ; { print $2 }')
 echo 'Successfully loaded image!'
 echo "Image Size: $hight x $width"
+
+declare -A matrix
+num_rows=$hight
+num_columns=$width
+
+function Pdataloop {
+    
+}
+
+
+for ((i=1;i<=num_rows;i++)) do
+    for ((j=1;j<=num_columns;j++)) do
+        matrix[$i,$j]=$RANDOM
+    done
+done
+
+f1="%$((${#num_rows}+1))s"
+f2=" %9s"
+
+printf "$f1" ''
+for ((i=1;i<=num_rows;i++)) do
+    printf "$f2" $i
+done
+echo
+
+
+#for ((j=1;j<=num_columns;j++)) do      ###TRANSPOSOES MATRIX###
+#    printf "$f1" $j
+#   for ((i=1;i<=num_rows;i++)) do
+#       printf "$f2" ${matrix[$i,$j]}
+#   done
+#  echo
+#done
