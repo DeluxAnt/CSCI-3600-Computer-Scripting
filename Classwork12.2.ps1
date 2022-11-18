a25cbce49f52cb87c896b25854b43bb2e6f09b5f
DeluxAnt <vandergriffbryce@gmail.com>
2022-11-17 22:27:36 -0500

    renamed:    Classword12.2.ps1 -> Classwork12.2.ps1

--- Classwork12.2.ps1
+++ Classwork12.2.ps1
@@ -1,8 +1,8 @@
 ﻿write-host "There are a total of $($args.count) arguments"
 if($args.Contains("-u")) { echo $env:USERNAME }
 if($args.Contains("-p")) { echo $pwd }
-#for ( $i = 0; $i -lt $args.Count; $i++) {
-#    Write-Host "Argument $i is $($args[$i])"
-#    
-#}
+for ( $i = 0; $i -lt $args.Count; $i++) {
+    Write-Host "Argument $i is $($args[$i])"
+    
+}
 
